c169b9f5e155b8a4de9ada4b140661bf8ac1c33f
mbezemer <mark.bezemer@gmail.com>
2017-09-23 18:57:10 +0200

    committing ex1

---
+++ ex1.py
@@ -0,0 +1,10 @@
+# This is a comment that does nothing.
+
+print "Hello World!"
+print "Hello Again."
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "I just added a new line."
