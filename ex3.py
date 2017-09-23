623bb07419ec313a0d926a5520a3b51293fadec7
mbezemer <mark.bezemer@gmail.com>
2017-09-23 19:14:01 +0200

    committing ex3

---
+++ ex3.py
@@ -0,0 +1,35 @@
+# This prints a text.
+print "I will now count my chickens:"
+
+# This prints Hens and the result of a calculation.
+print "Hens", 25 + 30 / 6
+# This prints Roosters and the result of a calculation.
+print "Roosters", 100 - 25 * 3 % 4
+
+# This prints a text.
+print "Now I will count the eggs:"
+
+# This prints a calculation.
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# This prints a text.
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# This prints a calculation.
+print 3 + 2 < 2 - 7
+
+# This shows a text with a calculation and the result.
+print "What is 3 + 2?", 3 + 2
+# This shows a text with a calculation and the result.
+print "What is 5 - 7?", 5 - 7
+
+# This prints a text.
+print "Oh, that's why its False."
+
+# This prints a text.
+print "How about some more."
+
+# This shows a text with a calculation and the result (three times).
+print "Is it greater?", 5 > - 2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
